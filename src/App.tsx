fbae773df3f949ab987ffc05018ddc70be451d2a
liuyue <liuyue2077@gmail.com>
2021-01-12 19:35:47 +0800

    feat: 基本功能布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import router from './router'
+
 function App() {
   return (
-    <div className="app container">
+    <div className="app container pt-3">
+      <Switch>
+        {
+          router.map(route => <Route path={route.path} component={route.component} />)
+        }
+      </Switch>
     </div>
   );
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,21 @@
+import { NavLink } from "react-router-dom";
+
 export default function Home() {
   return (
     <>
-      <h3></h3>
+      <h3 className="text-center pb-3">Awesome React Plugins</h3>
+      <ul className="list-group">
+        <li className="list-group-item d-flex align-items-center">
+          <p className="mb-0 flex-grow-1 text-success">文本描述文本描述文本描述文本描述文本描述文本描述文本描述文本描述文本描述文本描述</p>
+          <div className="text-nowrap">
+            <a href="" className="btn btn-primary btn-sm px-3 ml-3">npm</a>
+            <a href="" className="btn btn-primary btn-sm px-3 ml-3">GitHub</a>
+            <NavLink to="/">
+              <button className="btn btn-primary btn-sm px-3 ml-3">使用示例</button>
+            </NavLink>
+          </div>
+        </li>
+      </ul>
     </>
   )
 }
